909cf36dd304918962fe13ea0704a8cb2b155813
girakoo <girakoo@163.com>
2023-05-10 19:25:57 +0800

    feat: upload

---
+++ build/common.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+// 文件路径最大长度，部分老旧Windows可能限制在260。此处暂不考虑。
+#define MAX_FILE_PATH_LENGTH 1024
+
+// URL最大长度
+#define MAX_URL_LENGTH 1024
+
+#define MAX_SYMBOL_FILE_LOCAL_HISTORY_COUNT 10
+
+// 计算数组元素个数
+#define ELEMENT_OF(x) (sizeof(x) / sizeof(x[0]))
